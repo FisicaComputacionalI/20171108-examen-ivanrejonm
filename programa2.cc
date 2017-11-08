e197310abfa9ce433da1a467a0f636edfea8a0cf
ivanrejonm <31378786+ivanrejonm@users.noreply.github.com>
2017-11-08 12:52:03 -0600

    Programa2
    Programa2

---
+++ programa2.cc
@@ -0,0 +1,16 @@
+//programa2
+#include<iostream>
+using namespace std;
+int main()
+{
+  int x;
+  x=1;
+  while(x<=5){
+    if(x%3==0){
+      x=x+2;}
+    else{
+      cout<<x<<endl;
+      x=x+2;}
+  }
+  return 0;
+}
